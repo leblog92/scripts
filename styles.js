3301edf3dd7cb3d084013895a684744743a33223
leblog92 <50671954+leblog92@users.noreply.github.com>
2021-09-29 12:37:51 +0200

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded',function(){
+  if(!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://leblog92.github.io/scripts/atemulti.css">');
+},false);
